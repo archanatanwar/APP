3780429b4867c75e65e83348f1061231cb60dc57
archana tanwar <atanwar.1986@gmail.com>
2015-02-01 05:10:59 -0500

    Adding functionality to show output to the user

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -1,23 +1,57 @@
 package Game;
 
+//import java.awt.List;
+import java.util.*;
+
 public class Player {
 	String color;
 	String personality;
 	int minionHold;
 	int buildingHold;
-	int cash;
-	
+	int cashHold;
+	private String[] defaultRegions = { "Dolly Sisters", "The Scours",
+			"The Shades" };
+
+	Hashtable<String, RegionStatus> H_Region;
+	RegionStatus SP;
+	HashMap<String, List<Integer>> pCards;
+	List<Integer> list = new ArrayList<>();
+
 	public Player(String colorTemp, String personalityTemp) {
 		color = colorTemp;
 		personality = personalityTemp;
-		minionHold = 0;
-		buildingHold = 0;
-		cash = 0;
-		
+		minionHold = 12;
+		buildingHold = 6;
+		cashHold = 10;
+		H_Region = new Hashtable<String, RegionStatus>();
+		pCards = new HashMap<String, List<Integer>>();
+
+		for (int i = 1; i <= 5; i++) {
+			Pair pair = PlayerCards.returnPlayerCard();
+			String color_temp = pair.getCardColor();
+			int cardNo_temp = pair.getCard();
+			if (pCards.containsKey(color_temp)) {
+				list = pCards.get(color_temp);
+				list.add(cardNo_temp);
+			} else {
+				list.add(cardNo_temp);
+			}
+			pCards.put(color_temp, list);
+		}
 	}
-	public void initialPlayerStatus()
-	{
+
+	public void initialPlayerStatus() {
 		minionHold = minionHold - 3;
+		for (int i = 0; i < defaultRegions.length; i++) {
+			String temp = defaultRegions[i];
+			if (H_Region.containsKey(temp)) {
+				H_Region.get(defaultRegions[i]).placedMinion = 3;
+			} else {
+				SP = new RegionStatus();
+				SP.placedbuilding = 0;
+				SP.placedMinion = 3;
+				H_Region.put(defaultRegions[i], SP);
+			}
+		}
 	}
-	
 }
--- src/Game/RegionStatus.java
+++ src/Game/RegionStatus.java
@@ -1,53 +1,6 @@
 package Game;
-import java.util.Hashtable;
-/**
- * 
- */
 
-/**
- * @author Archana
- *
- */
 public class RegionStatus {
-	String rName;
-	int rNumber;
-	int buildingCost;
-	int rMinionNum;
-	int troubleMarker;
-	int demon;
-	int troll;
-	Hashtable<String,PlayerStatus> H_Player;
-	PlayerStatus SP;
-	public RegionStatus(String Name, int Number, int bCost) {
-		rName = Name;
-		rNumber = Number;
-		buildingCost = bCost;
-		rMinionNum = 0;
-		troubleMarker = 0;
-		demon = 0;
-		troll = 0;
-		H_Player = new Hashtable<String, PlayerStatus>();
-	}
-	
-	public void placeDefaultMinion(String color)
-	{
-		rMinionNum = rMinionNum + 1;
-		if (rMinionNum > 1)
-		{
-			troubleMarker = 1;
-		}
-		if (H_Player.containsKey(color)) {
-			H_Player.get(color).pMinionRegionwise = rMinionNum;
-		}
-		else
-		{
-			SP = new PlayerStatus();
-			SP.color = color;
-			SP.pMinionRegionwise = rMinionNum;
-			SP.pbuildingRegionwise = 0;			
-			H_Player.put(color, SP);
-		}
-	}
-
-
+		int placedMinion;
+		int placedbuilding;
 }
